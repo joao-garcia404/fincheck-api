03cafcc2e4151c6f8332d157e8d25402f7cc9391
Joao Garcia <jvdasilvaeirasgarcia@gmail.com>
2023-08-19 19:04:42 -0300

    Feat: Authentication

---
+++ src/modules/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+
+import { env } from 'src/shared/config/env';
+
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+
+@Module({
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: env.jwtSecret,
+      signOptions: { expiresIn: '7d' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class AuthModule {}
